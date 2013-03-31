6142c8c1bc19541d0a89ce404d166cdb0b03e0d6
Marcus Karlsson <mk@acc.umu.se>
2013-03-31 17:25:46 +0200

    Enable controls depending on server state
    All controls are always enabled, no matter if they can be used or not. We
    should enable and disable controls accordingly when the server becomes
    enabled or disabled.
    
    Signed-off-by: Marcus Karlsson <mk@acc.umu.se>

--- Muu/MainWindow.xaml.vb
+++ Muu/MainWindow.xaml.vb
@@ -6,6 +6,7 @@
         InitializeComponent()
 
         PortBox.Text = port
+        SetControlState(False)
 
         server = New Server()
         server.ServerEnabled = AddressOf ServerEnabled
@@ -14,10 +15,12 @@
 
     Private Sub ServerEnabled()
         Log("Server enabled")
+        SetControlState(True)
     End Sub
 
     Private Sub ServerDisabled()
         Log("Server disabled")
+        SetControlState(False)
     End Sub
 
     Private Sub EnableButton_Click(sender As Object, e As RoutedEventArgs)
@@ -54,5 +57,17 @@
             LogBox.AppendText(Environment.NewLine)
         End If
         LogBox.AppendText(message)
+    End Sub
+
+    Private Sub SetControlState(serverEnabled As Boolean)
+        If serverEnabled Then
+            EnableButton.IsEnabled = False
+            DisableButton.IsEnabled = True
+            PortBox.IsEnabled = False
+        Else
+            EnableButton.IsEnabled = True
+            DisableButton.IsEnabled = False
+            PortBox.IsEnabled = True
+        End If
     End Sub
 End Class
